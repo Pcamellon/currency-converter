32dd58d6745c8f66de50849fa5fd05e10592dcf5
Pedro P. Camellon <pedropcamellon@gmail.com>
2022-10-05 07:55:51 -0400

    Add reference from js file to html elements

--- src/index.js
+++ src/index.js
@@ -1,56 +1,69 @@
-const axios = require("axios");
+// Libs
+// const axios = require("axios");
 
-const getExchangeRate = async (fromCurrency, toCurrency) => {
-  try {
-    const response = await axios.get(
-      "http://data.fixer.io/api/latest?access_key=f68b13604ac8e570a00f7d8fe7f25e1b&format=1"
-    );
+// HTML Elements
+const fromCurrencyElem = document.querySelector("#from");
+const toCurrencyElem = document.querySelector("#to");
+const fromQtyElem = document.querySelector("#from-qty");
+const toQtyElem = document.querySelector("#to-qty");
 
-    const rate = response.data.rates;
-    const euro = 1 / rate[fromCurrency];
-    const exchangeRate = euro * rate[toCurrency];
+// const getExchangeRate = async (fromCurrency, toCurrency) => {
+//   try {
+//     const response = await axios.get(
+//       "http://data.fixer.io/api/latest?access_key=f68b13604ac8e570a00f7d8fe7f25e1b&format=1"
+//     );
 
-    return exchangeRate;
-  } catch (error) {
-    throw new Error(
-      `Unable to get currency ${fromCurrency} and  ${toCurrency}`
-    );
-  }
+//     const rate = response.data.rates;
+//     const euro = 1 / rate[fromCurrency];
+//     const exchangeRate = euro * rate[toCurrency];
+
+//     return exchangeRate;
+//   } catch (error) {
+//     throw new Error(
+//       `Unable to get currency ${fromCurrency} and  ${toCurrency}`
+//     );
+//   }
+// };
+
+// const getCountries = async (currencyCode) => {
+//   try {
+//     const response = await axios.get(
+//       `https://restcountries.eu/rest/v2/currency/${currencyCode}`
+//     );
+
+//     return response.data.map((country) => country.name);
+//   } catch (error) {
+//     throw new Error(`Unable to get countries that use ${currencyCode}`);
+//   }
+// };
+
+const convertCurrency = async () => {
+  // let exchangeRate = -1;
+  // let countries = [];
+
+  const fromCurrency = fromCurrencyElem.value;
+  const toCurrency = toCurrencyElem.value;
+
+  let fromQty = parseFloat(fromQtyElem.value);
+  let toQty = parseFloat(toQtyElem.value);
+
+  // toQty.value = fromQty * exchangeRate;
+
+  // await Promise.all([
+  //   getExchangeRate(fromCurrency, toCurrency),
+  //   getCountries(toCurrency)
+  // ]).then(([exchangeRateValue, countriesValue]) => {
+  //   exchangeRate = exchangeRateValue;
+  //   countries = countriesValue;
+  // });
+  // const convertedAmount = (amount * exchangeRate).toFixed(2);
+  // return `${amount} ${fromCurrency} is worth ${convertedAmount} ${toCurrency}. You can spend these in the following countries: ${countries}`;
 };
 
-const getCountries = async (currencyCode) => {
-  try {
-    const response = await axios.get(
-      `https://restcountries.eu/rest/v2/currency/${currencyCode}`
-    );
-
-    return response.data.map((country) => country.name);
-  } catch (error) {
-    throw new Error(`Unable to get countries that use ${currencyCode}`);
-  }
-};
-
-const convertCurrency = async (fromCurrency, toCurrency, amount) => {
-  let exchangeRate;
-  let countries;
-
-  await Promise.all([
-    getExchangeRate(fromCurrency, toCurrency),
-    getCountries(toCurrency)
-  ]).then(([exchangeRateValue, countriesValue]) => {
-    exchangeRate = exchangeRateValue;
-    countries = countriesValue;
-  });
-
-  const convertedAmount = (amount * exchangeRate).toFixed(2);
-
-  return `${amount} ${fromCurrency} is worth ${convertedAmount} ${toCurrency}. You can spend these in the following countries: ${countries}`;
-};
-
-convertCurrency("USD", "CAD", 20)
-  .then((message) => {
-    console.log(message);
-  })
-  .catch((error) => {
-    console.log(error.message);
-  });
+convertCurrency();
+// .then((message) => {
+//   console.log(message);
+// })
+// .catch((error) => {
+//   console.log(error.message);
+// });
