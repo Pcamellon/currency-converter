6384d327131f0de3a267a1cf38f97b9d12e62738
Pedro P. Camellon <pedropcamellon@gmail.com>
2022-10-05 10:54:13 -0400

    Call convertCurrency fn when qty input value changes

--- src/index.js
+++ src/index.js
@@ -1,11 +1,17 @@
 // Libs
-// const axios = require("axios");
+// import { get } from "axios";
 
 // HTML Elements
 const fromCurrencyElem = document.querySelector("#from");
 const toCurrencyElem = document.querySelector("#to");
 const fromQtyElem = document.querySelector("#from-qty");
-const toQtyElem = document.querySelector("#to-qty");
+// const toQtyElem = document.querySelector("#to-qty");
+
+//
+// const API_KEY = `6OAfpJGCnjvjgHXXFAb5yUko7L2YweJt`;
+// const API = `https://api.exchangerate.host/convert`;
+
+// const FIXER_API = `http://data.fixer.io/api/latest?access_key=${FIXER_API_KEY}`;
 
 // const getExchangeRate = async (fromCurrency, toCurrency) => {
 //   try {
@@ -37,33 +43,96 @@ const toQtyElem = document.querySelector("#to-qty");
 //   }
 // };
 
-const convertCurrency = async () => {
+const convertCurrency = (e) => {
+  // TODO
+  // Check what changed :  e.target.id  == from-qty {} ...
+
+  const fromQty = e.target.value;
+
+  console.log(`fromQty: ${fromQty}`);
+
   // let exchangeRate = -1;
-  // let countries = [];
+  // // let countries = [];
 
-  const fromCurrency = fromCurrencyElem.value;
-  const toCurrency = toCurrencyElem.value;
+  // const fromCurrency = fromCurrencyElem.value;
+  // const toCurrency = toCurrencyElem.value;
 
-  let fromQty = parseFloat(fromQtyElem.value);
-  let toQty = parseFloat(toQtyElem.value);
+  // let fromQty = parseFloat(fromQtyElem.value);
+  // let toQty = parseFloat(toQtyElem.value);
 
-  // toQty.value = fromQty * exchangeRate;
+  // let headers = new Headers();
+  // headers.append("apikey", API_KEY);
+
+  // let requestOptions = {
+  //   method: "GET",
+  //   // redirect: "follow"
+  //   // headers: headers
+  // };
+
+  // const response = await fetch(
+  //   `${API}?from=${fromCurrency}&to=${toCurrency}`,
+  //   requestOptions
+  // );
+  // .then((response) => response.text())
+  // .then((result) => console.log(result))
+  // .catch((error) => console.log("error", error));
+
+  // let { info } = JSON.parse(await response.text());
+
+  // return response;
+
+  // try {
+  //   const response = await get(FIXER_API);
+  //   console.log(response);
+  // } catch (error) {
+  //   throw new Error(`Unable to get currency ${error}`);
+  // }
+
+  // console.log(response.data);
 
   // await Promise.all([
+  //   async () => {
+  // const response = await axios.get(
+  //   "http://data.fixer.io/api/latest?access_key=f68b13604ac8e570a00f7d8fe7f25e1b&format=1"
+  // );
+
+  // console.log(response.data);
+
+  // exchangeRate = info["rate"];
+  // const euro = 1 / rate[fromCurrency];
+  // const exchangeRate = euro * rate[toCurrency];
+
+  // return exchangeRate;
+  // return "exchangeRate";
+
+  // try {
+  //
+  // } catch (error) {
+  // throw new Error(
+  //   `Unable to get currency ${fromCurrency} and  ${toCurrency}`
+  // );
+  // }
+  // }
+  // ]);
+
+  //   await Promise.all([
   //   getExchangeRate(fromCurrency, toCurrency),
   //   getCountries(toCurrency)
   // ]).then(([exchangeRateValue, countriesValue]) => {
   //   exchangeRate = exchangeRateValue;
   //   countries = countriesValue;
   // });
-  // const convertedAmount = (amount * exchangeRate).toFixed(2);
-  // return `${amount} ${fromCurrency} is worth ${convertedAmount} ${toCurrency}. You can spend these in the following countries: ${countries}`;
+
+  // toQty = (fromQty * exchangeRate).toFixed(2);
+  // toQtyElem.value = fromQty * exchangeRate;
 };
 
-convertCurrency();
+fromQtyElem.addEventListener("change", convertCurrency);
 // .then((message) => {
 //   console.log(message);
-// })
+// });
 // .catch((error) => {
 //   console.log(error.message);
 // });
+
+// console.log(convertCurrency());
